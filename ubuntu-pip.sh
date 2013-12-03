8652a427b8e82e2e0a25e5eb3546e1a35192d376
Roy Inganta Ginting <if09062@gmail.com>
2013-12-03 11:10:45 +0700

    Update ubuntu-pip.sh
    Alternative to install latest version

--- ubuntu-pip.sh
+++ ubuntu-pip.sh
@@ -4,4 +4,11 @@
 # Pip is package management system used to install and manage software written in python
 # List of available packages can be found on https://pypi.python.org/pypi
 
-sudo apt-get install -y python-pip
+# Via apt-get, will install old version
+#sudo apt-get install -y python-pip
+
+# Latest version from http://www.pip-installer.org/en/latest/installing.html#install-or-upgrade-pip
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+python get-pip.py
+
+
