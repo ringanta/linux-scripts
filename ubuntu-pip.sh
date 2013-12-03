2441a7f90a2d00ab492564abc229ffbbe4a167ef
Roy Inganta Ginting <if09062@gmail.com>
2013-12-03 10:54:53 +0700

    Create ubuntu-pip.sh

---
+++ ubuntu-pip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install python pip with apt-get
+# Pip is package management system used to install and manage software written in python
+# List of available packages can be found on https://pypi.python.org/pypi
+
+sudo apt-get install -y python-pip
