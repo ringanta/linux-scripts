52543aae42dde79b692a3a1a32fa3e1c21238bce
Roy Inganta Ginting <if09062@gmail.com>
2014-01-28 16:23:46 +0700

    Update ubuntu-git.sh

--- ubuntu-git.sh
+++ ubuntu-git.sh
@@ -3,7 +3,7 @@
 # Install git from ppa
 # base on https://launchpad.net/~git-core/+archive/ppa
 
-sudo add-apt-repository ppa:git-core/ppa
+sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt-get update
 
 sudo apt-get install -y git
